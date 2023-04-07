ef5d804597aeaef34c9c7e7344d45e2792b8aa8f
ModernMediaLLC <124941890+ModernMediaLLC@users.noreply.github.com>
2023-04-07 16:47:22 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const context = canvas.getContext("2d");
-const frameCount = 60;
+const frameCount = 30;
 
 const currentFrame = (index) => `./header/${(index + 1).toString()}.jpg`;
 
