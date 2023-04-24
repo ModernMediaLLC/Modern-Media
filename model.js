a6b97fc3524fc4a9c1fe9a692f4d7af304395a40
ModernMediaLLC <124941890+ModernMediaLLC@users.noreply.github.com>
2023-04-24 15:56:47 -0700

    Create model.js

---
+++ model.js
@@ -0,0 +1,10 @@
+var MAX_MODELS = 3;
+var numModelsPreviewed = 0;
+
+function previewModel() {
+  // Add code to preview the model and increment numModelsPreviewed
+}
+
+function removeModel() {
+  // Add code to remove the model and decrement numModelsPreviewed
+}
